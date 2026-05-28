c530046d30eadbefeed74bc91f32d401715bf891
Guy Sharabi <uas0035507@stud.fra-uas.de>
2026-05-28 20:51:58 +0200

    fixed naming error

--- Main/Main.py
+++ Main/Main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello, World! Your FastAPI is working!"}
 
-# 3. Define another route with a path parameter
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
     return {"item_id": item_id, "query_param": q}
